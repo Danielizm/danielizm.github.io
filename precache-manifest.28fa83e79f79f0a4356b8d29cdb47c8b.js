451b827d8be598fe0c51464e3b7d11d065e4ca7d
defector <daniel@thedefectors.com.au>
2019-10-16 18:07:06 +1100

    Updates

---
+++ precache-manifest.28fa83e79f79f0a4356b8d29cdb47c8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8b340a4f238e1f8654cd2b1035432a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8f1cd5edbd122bd36ee6",
+    "url": "/test/static/css/main.1a942d58.chunk.css"
+  },
+  {
+    "revision": "c4247fd56eaf38d16145",
+    "url": "/test/static/js/2.d7d4af98.chunk.js"
+  },
+  {
+    "revision": "8f1cd5edbd122bd36ee6",
+    "url": "/test/static/js/main.ff311f57.chunk.js"
+  },
+  {
+    "revision": "d51128ee3113cf026fe3",
+    "url": "/test/static/js/runtime~main.80713d17.js"
+  }
+]);
